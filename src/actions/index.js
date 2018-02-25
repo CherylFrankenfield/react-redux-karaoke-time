818e5844d30dbacf111e58841a4a4f0496a4d780
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-24 20:24:05 -0800

    Add test for newStateEntry created when API lyrics are being requested

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -14,3 +15,17 @@ export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=8c541ea7b82482967149f352f33e6dbd').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });
+  };
+}
