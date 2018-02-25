b52a46ba3e701cfc34f478aad275c3d634ff1e3f
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-25 10:20:32 -0800

    Add test Update state on receive song and fails

--- __tests__/reducers/index.js
+++ __tests__/reducers/index.js
@@ -37,6 +37,21 @@ describe('Karaoke App', () => {
       .toEqual(newStateEntry);
     });
 
+    it('Should update state on receive song', () => {
+      const action = actions.receiveSong('kiss', 'prince', 1, ['you don\'t have to be beautiful', 'to turn me on']);
+      const newObject = {
+        isFetching: false,
+        title: action.title,
+        artist: action.artist,
+        songId: action.songId,
+        receivedAt: action.receivedAt,
+        songArray: action.songArray,
+        arrayPosition: 0
+      };
+      expect(lyricChangeReducer(initialState.songsById, action)[action.songId])
+      .toEqual(newObject);
+    });
+
   });
 
   describe('songChangeReducer', () => {
